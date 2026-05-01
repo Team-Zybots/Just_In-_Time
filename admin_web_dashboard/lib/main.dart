347f7c237ba7e1236bb86c660f395bd2ab8d3301
Devindi Wickramarathna <devwickramarathna@gmail.com>
2026-05-01 15:29:33 +0530

    Updated main.dart for admin dashboard

--- admin_web_dashboard/lib/main.dart
+++ admin_web_dashboard/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:admin_web_dashboard/screens/live_queue_screen.dart';
+import 'package:admin_web_dashboard/screens/payments_screen.dart';
+import 'package:admin_web_dashboard/screens/manage_doctors_screen.dart';
 void main() {
   runApp(const AdminDashboardApp());
 }
@@ -39,8 +41,10 @@ class _AdminShellState extends State<AdminShell> {
 
   // These are the screens the admin can switch between
   final List<Widget> _screens = [
-    //
-  ];
+  const LiveQueueScreen(), 
+  const ManageDoctorsScreen(),
+  const PaymentsScreen(), // <-- THE FINAL SCREEN IS CONNECTED
+];
 
   @override
   Widget build(BuildContext context) {
